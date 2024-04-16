9c3393c8fcd721843b246dad933bd8f957317f16
Wallblue <nrb.busquet@gmail.com>
2024-04-16 20:59:50 +0200

    Add debug zone with prefabs and death zones

---
+++ Assets/MaximeAssets/Scripts/WindInteraction.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+public class WindInteraction : MonoBehaviour
+{
+    public Rigidbody bodyRigidBody;
+    public float windForce = 1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
