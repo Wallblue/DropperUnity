0b9054209fb85bf28f7767b26de4ea7b65e705ab
Wallblue <nrb.busquet@gmail.com>
2024-04-21 19:35:20 +0200

    Apply the moving things 'relativity' to everything in script

--- Assets/Scripts/PlatformScript.cs
+++ Assets/Scripts/PlatformScript.cs
@@ -4,21 +4,11 @@ public class PlatformScript : MonoBehaviour
 {
     private void OnCollisionEnter(Collision other)
     {
-        if (other.rigidbody.gameObject.layer != LayerMask.NameToLayer("Player"))
-        {
-            return;
-        }
-
         other.rigidbody.gameObject.transform.parent = gameObject.transform;
     }
 
     private void OnCollisionExit(Collision other)
     {
-        if (other.rigidbody.gameObject.layer != LayerMask.NameToLayer("Player"))
-        {
-            return;
-        }
-
         other.rigidbody.gameObject.transform.parent = null;
     }
 }
